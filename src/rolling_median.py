6f7ecb06de0f6e58d57224549e05b404b4296218
Daniel Ong <danielong1@gmail.com>
2016-07-10 16:42:00 -0700

    Fixed time calculations

--- src/rolling_median.py
+++ src/rolling_median.py
@@ -4,11 +4,14 @@ import datetime
 import uuid
 import json
 
+DURATION = datetime.timedelta(seconds=60)
+
 class Graph(object):
     """ Simple graph implementation using dicts """
     def __init__(self, in_graph_dict={}):
         """ Initialize graph. Empty by default """
         self.__graph_dict = in_graph_dict
+        self.__latest_timestamp = datetime.datetime(1901, 1, 1, 0, 0, 0)
         
     def vertices(self):
         """ Return list of vertices """
@@ -25,24 +28,28 @@ class Graph(object):
             
     def remove_edge(self, edge):
         """ Delete an edge from the graph """
-        pass
-    
-    def remove_vertex(self, edge):
-        """ Delete a vertex from the graph """
-        pass
-    
-            
-    def add_edge(self, edge, timestamp):
-        """ Add edge between two vertices. An edge is a set/tuple/list of two vertices and a timestamp """
         (vertex1, vertex2) = tuple(edge)
         if vertex1 in self.__graph_dict:
-            self.__graph_dict[vertex1].append(vertex2)
-        else:
-            self.__graph_dict[vertex1] = [vertex2]
-        if vertex2 in self.__graph_dict:
-            self.__graph_dict[vertex2].append(vertex1)
-        else:
-            self.__graph_dict[vertex2] = [vertex1]
+            pass
+    def remove_vertex(self, vertex):
+        """ Delete a vertex from the graph """
+        pass
+            
+    def add_edge(self, edge, timestamp):
+        """ Add edge between two vertices. An edge is a set/tuple/list of two vertices and a timestamp. This is skipped if the timestamp is 60 seconds before latest timestamp. If the new edge has the latest timestamp, we remove all edges whose timestamps are 60 seconds or more before the new latest timestamp """
+        print(self.__latest_timestamp - timestamp)
+        if timestamp > self.__latest_timestamp or (self.__latest_timestamp - timestamp > DURATION):
+            (vertex1, vertex2) = tuple(edge)
+            if vertex1 in self.__graph_dict:
+                self.__graph_dict[vertex1].append((vertex2, timestamp))
+            else:
+                self.__graph_dict[vertex1] = [(vertex2, timestamp)]
+            if vertex2 in self.__graph_dict:
+                self.__graph_dict[vertex2].append((vertex1, timestamp))
+            else:
+                self.__graph_dict[vertex2] = [(vertex1, timestamp)]
+                
+            if 
             
     def __generate_edges(self):
         """ Generate all edges in graph """
@@ -57,6 +64,9 @@ class Graph(object):
         """ Returns the degree of a vertex """
         adj_vertices = self.__graph_dict[vertex]
         degree = len(adj_vertices) + adj_vertices.count(vertex)
+        # remove disconnected nodes
+        if degree == 0:
+            self.remove_vertex(vertex)
         return degree
     
     def median_degree(self):
@@ -88,6 +98,10 @@ def parse_line(json_line, g):
     g.add_edge([json_line['actor'], json_line['target']], parse_timestamp(json_line['created_time']))
     
 if __name__ == "__main__":
+    dt1 = parse_timestamp('2016-04-07T03:33:19Z')
+    dt2 = parse_timestamp('2016-04-07T03:34:19Z')
+    if (dt2-dt1) > DURATION:
+        print("should see this")
     # get input and output files
     try:
         inFile = sys.argv[1]
@@ -103,9 +117,6 @@ if __name__ == "__main__":
             for line in f:
                 parse_line(json.loads(line), g)
                 data.append(json.loads(line))
-                print("Median degree: ", g.median_degree())
+                print("Median degree: {:.2f}".format(g.median_degree()))
     except IOError:
         print("Could not read file:", inFile)
-    
-    print(data[0]['created_time'])
-    
